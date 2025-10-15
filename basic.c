c685a552be9f1a1b5d19bd1235a857ef7e700eac
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-15 16:31:19 +0530

    Writing the function here(written  a simple C program that handles Ctrl+C (SIGINT) signal.  When the user presses Ctrl+C, the program catches the signal and exits  instead of stopping suddenly)

--- basic.c
+++ basic.c
@@ -3,3 +3,31 @@
 #include <signal.h>
 #include <unistd.h>
 
+void handle_sigint(int sig) {
+    printf("\nCaught signal %d (Ctrl+C pressed). Exiting gracefully.\n", sig);
+    _exit(0);
+}
+
+int main() {
+    signal(SIGINT, handle_sigint);
+    printf("Running... Press Ctrl+C to stop.\n");
+
+    while(1) {
+        sleep(1);
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
