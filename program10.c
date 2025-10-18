2d4109bd0fc89f5b532f39827a38834b2040cc5e
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-18 17:59:10 +0530

    Implemented multithreading with pthreads in program10.c

--- program10.c
+++ program10.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <pthread.h>
+void* printMessage(void* arg) {
+    printf("Hello from the thread!\n");
+    return NULL;
+}
 
+int main() {
+    pthread_t thread;
+    pthread_create(&thread, NULL, printMessage, NULL);
+    pthread_join(thread, NULL);
+    printf("Thread has finished execution.\n");
+    return 0;
+}
 
 
