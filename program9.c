8fa67e23e1f50ff7b93960466ddd4ff1cf525d4a
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-18 17:54:45 +0530

    Added fork and exec functionality in program9.c
    Implement process creation using fork() and executed ls command in child process.

--- program9.c
+++ program9.c
@@ -3,6 +3,27 @@
 #include <unistd.h>      
 #include <sys/types.h>
 #include <sys/wait.h>    
+int main() {
+    pid_t pid;
+    pid = fork();
+
+    if (pid < 0) {
+        printf("Error in creating a child process\n");
+        exit(1);
+    }
+    else if (pid > 0) {
+        printf("Parent Process\nParent PID = %d\nChild PID = %d\n", getpid(), pid);
+        wait(NULL);  
+        printf("Parent resumes\n");
+    }
+    else {
+        printf("Child Process\n");
+        printf("Parent PID = %d, Child PID = %d\n", getppid(), getpid());
+       execlp("/bin/ls", ls, NULL);
+      }
+    return 0;
+}
+
 
 
 
