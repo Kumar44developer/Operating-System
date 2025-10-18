bb5c31896e9ac5768121810a12ff0f20ab382da3
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-18 12:26:59 +0530

    Implemented  exec with execlp to list directory

--- Program8.c
+++ Program8.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
 
+int main() {
+    printf("Before exec\n");
+    execlp("ls", "ls", "-l", NULL);
+    perror("execlp failed");  // Only runs if execlp fails
+    return 0;
+}
