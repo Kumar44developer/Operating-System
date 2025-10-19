377e705b1c99ce20f120893f29f169c86b790b17
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-19 11:08:25 +0530

    Implemented multithreaded sum calculation

--- program14.c
+++ program14.c
@@ -1,4 +1,33 @@
 #include <stdio.h>
 #include <pthread.h>
+int sum = 0;
+pthread_mutex_t lock;
 
+void* addNumbers(void* arg) {
+    int n = *(int*)arg;
+    pthread_mutex_lock(&lock);
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+    int n1 = 5, n2 = 10;
+
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&t1, NULL, addNumbers, &n1);
+    pthread_create(&t2, NULL, addNumbers, &n2);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Final sum = %d\n", sum);
+
+    pthread_mutex_destroy(&lock);
+    return 0;
+}
 
