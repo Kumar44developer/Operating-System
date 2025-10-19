2a8452f2f71e68f4f86dbda437ae3d3ebff9a175
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-19 11:06:35 +0530

    Implemented multi-threading with pthreads

--- program13.c
+++ program13.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
 #include <pthread.h>
+
+void* printTask(void* arg) {
+    int id = *(int*)arg;
+    printf("Thread %d is running.\n", id);
+    return NULL;
+}
+
+int main() {
+    pthread_t threads[3];
+    int ids[3] = {1, 2, 3};
+
+    for (int i = 0; i < 3; i++) {
+        pthread_create(&threads[i], NULL, printTask, &ids[i]);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("All threads finished.\n");
+    return 0;
+}
