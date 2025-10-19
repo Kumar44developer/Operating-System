4034c22ee5d413f3e2a6a54093e2fb4b441575ab
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-19 11:04:24 +0530

    Added multi-threading example using pthreads

--- program12.c
+++ program12.c
@@ -1,7 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<unistd.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <pthread.h>
 
+void* printNumber(void* arg) {
+    int num = *(int*)arg;
+    printf("Number from thread: %d\n", num);
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    int value = 42;
+
+    pthread_create(&thread, NULL, printNumber, &value);
+    pthread_join(thread, NULL);
+
+    printf("Main thread completed.\n");
+    return 0;
+}
 
