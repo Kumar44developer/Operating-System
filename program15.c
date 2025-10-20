89ad32614b5b5c985e45ee23c714d4309b18572d
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-20 09:49:40 +0530

    Implemented file creation and content display
    Create and write to a file, then read its content.

--- program15.c
+++ program15.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+int main() {
+    int fd;
+    char buffer[100];
+
+    fd = open("sample.txt", O_CREAT | O_RDWR, 0777);
+    write(fd, "Operating Systems Concepts\n", 28);
+
+    lseek(fd, 0, SEEK_SET);
+    read(fd, buffer, 28);
+
+    printf("File Content: %s\n", buffer);
+    close(fd);
+    return 0;
+}
 
