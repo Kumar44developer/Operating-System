35d50ed53ea14ace26b78a0401e932b723dc7d1c
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-20 09:54:03 +0530

    Implemented thread cleanup with pthread_cleanup_push
    Added thread cleanup functionality with resource management.

--- program17.c
+++ program17.c
@@ -2,4 +2,20 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void cleanup(void *arg) {
+    printf("Cleaning up resources: %s\n", (char *)arg);
+}
+
+void *thread_func(void *arg) {
+    pthread_cleanup_push(cleanup, "Thread resources");
+    
+    for (int i = 0; i < 10; i++) {
+        printf("Thread running %d\n", i);
+        sleep(1);  
+    }
+    
+    pthread_cleanup_pop(1);  
+    return NULL;
+}
+
 
