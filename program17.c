863499a598faaa6ef552a7839c98c39255f74155
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-20 09:54:35 +0530

    Added main function to create and cancel a thread

--- program17.c
+++ program17.c
@@ -17,5 +17,16 @@ void *thread_func(void *arg) {
     pthread_cleanup_pop(1);  
     return NULL;
 }
+int main() {
+    pthread_t tid;
+    pthread_create(&tid, NULL, thread_func, NULL);
+
+    sleep(3);
+    pthread_cancel(tid);  
+
+    pthread_join(tid, NULL);
+    printf("Thread has been canceled\n");
+    return 0;
+}
 
 
