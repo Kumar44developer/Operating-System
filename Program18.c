c89be094881e415005ed0c88e7c8ad35e1ae54ae
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-25 13:18:10 +0530

    Implemented basic file I/O using system calls
    Implemented basic file I/O using system calls
    
    Added program demonstrating open, read, write, lseek
    
    Created file handling example in C
    
    Added sample program for file operations
    
    Demonstrated file read/write with system calls

--- Program18.c
+++ Program18.c
@@ -2,3 +2,17 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+int main() {
+    int fd;
+    char buffer[100];
+
+    fd = open("sample.txt", O_CREAT | O_RDWR, 0777);
+    write(fd, "Operating Systems Concepts\n", 28);
+
+    lseek(fd, 0, SEEK_SET);
+    read(fd, buffer, 28);
+
+    printf("File Content: %s\n", buffer);
+    close(fd);
+    return 0;
+}
