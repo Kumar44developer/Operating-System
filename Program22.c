5092c3d5f137db3da56a8d8aeda726921f076e08
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-29 15:28:32 +0530

    Implemented arrival and burst time input with FCFS sorting

--- Program22.c
+++ Program22.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, bt[20], at[20], wt[20], tat[20], ct[20];
+    float awt = 0, atat = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter Arrival Time and Burst Time for P%d: ", i + 1);
+        scanf("%d%d", &at[i], &bt[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (at[i] > at[j]) {
+                int temp = at[i]; at[i] = at[j]; at[j] = temp;
+                temp = bt[i]; bt[i] = bt[j]; bt[j] = temp;
+            }
+        }
+    }
 
