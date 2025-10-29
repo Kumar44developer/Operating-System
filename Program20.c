52cd8c0d13d12ec8e6b35e424736edfc8f9806a7
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-29 15:26:41 +0530

    Updated the correct logic to the file

--- Program20.c
+++ Program20.c
@@ -12,7 +12,29 @@ int main() {
         printf("P%d: ", i + 1);
         scanf("%d", &bt[i]);
     }
-  wt[0] = 0; // First process has no waiting time
+  wt[0] = 0; #include <stdio.h>
+
+int main() {
+    int n, bt[20], at[20], wt[20], tat[20], ct[20];
+    float awt = 0, atat = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter Arrival Time and Burst Time for P%d: ", i + 1);
+        scanf("%d%d", &at[i], &bt[i]);
+    }
+
+  
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (at[i] > at[j]) {
+                int temp = at[i]; at[i] = at[j]; at[j] = temp;
+                temp = bt[i]; bt[i] = bt[j]; bt[j] = temp;
+            }
+        }
+    }
     for (int i = 1; i < n; i++)
         wt[i] = wt[i - 1] + bt[i - 1];
 
