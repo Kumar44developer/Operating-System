44eb66bf75f654f95295178d77d55eff002597d1
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-29 15:24:05 +0530

    Added waiting time and turnaround time calculation Logic Here

--- Program20.c
+++ Program20.c
@@ -12,3 +12,22 @@ int main() {
         printf("P%d: ", i + 1);
         scanf("%d", &bt[i]);
     }
+  wt[0] = 0; // First process has no waiting time
+    for (int i = 1; i < n; i++)
+        wt[i] = wt[i - 1] + bt[i - 1];
+
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+        awt += wt[i];
+        atat += tat[i];
+    }
+
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++)
+        printf("P%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+
+    printf("\nAverage Waiting Time: %.2f", awt / n);
+    printf("\nAverage Turnaround Time: %.2f\n", atat / n);
+
+    return 0;
+}
