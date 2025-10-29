8532cf1d91d040410647cc09b7effc4435d1469e
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-29 15:22:42 +0530

    Added code to read number of processes and burst times in this part

--- Program20.c
+++ Program20.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int n, bt[20], wt[20], tat[20];
+    float awt = 0, atat = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter Burst Time for each process:\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d: ", i + 1);
+        scanf("%d", &bt[i]);
+    }
