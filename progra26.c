c765e960b4eb4345c0297643d2a417fc8c0593e0
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-01 13:29:44 +0530

    Added FCFS CPU Scheduling program with arrival and burst time input

--- progra26.c
+++ progra26.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int n, at[20], bt[20], ct[20], wt[20], tat[20];
+    float awt = 0, atat = 0, cpu_utilization;
+    int total_idle = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter Arrival Time and Burst Time for P%d: ", i + 1);
+        scanf("%d%d", &at[i], &bt[i]);
+    }
