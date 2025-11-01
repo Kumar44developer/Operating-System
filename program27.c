259f3cbb685f7864f4cef5bf88f2c30ab1c26f49
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-01 13:36:30 +0530

    Added sorting logic to arrange processes by burst time for SJF scheduling

--- program27.c
+++ program27.c
@@ -13,3 +13,12 @@ int main() {
         scanf("%d", &bt[i]);
         p[i] = i + 1;
     }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (bt[i] > bt[j]) {
+                temp = bt[i]; bt[i] = bt[j]; bt[j] = temp;
+                temp = p[i]; p[i] = p[j]; p[j] = temp;
+            }
+        }
+    }
