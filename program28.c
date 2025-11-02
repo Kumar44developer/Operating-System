07873973e1f49237c58148dd96143661cf052ad4
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-02 16:06:13 +0530

    Added Non-Preemptive SJF CPU Scheduling program (all processes arrive at same time)

--- program28.c
+++ program28.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int n, bt[20], wt[20], tat[20], p[20];
+    float awt = 0, atat = 0;
+    int temp;
+
+    printf("Enter total number of processes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter Burst Time for process %d: ", i + 1);
+        scanf("%d", &bt[i]);
+        p[i] = i + 1;
+    }
