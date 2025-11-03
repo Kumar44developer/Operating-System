3c286e88843e97b3b04999b2a9c52306d73c9c6c
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-03 08:57:02 +0530

    Added process input section for SJF Non-Preemptive scheduling with arrival and burst times

--- program30.c
+++ program30.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int n, bt[20], at[20], wt[20], tat[20], ct[20], completed[20] = {0};
+    float awt = 0, atat = 0;
+    int time = 0, count = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter Arrival Time and Burst Time for P%d: ", i + 1);
+        scanf("%d%d", &at[i], &bt[i]);
+    }
