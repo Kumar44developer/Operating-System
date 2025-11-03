33ec07aaf91a67c4ed341e0a19286a486585b44c
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-03 08:59:41 +0530

    Added SJF scheduling logic and Gantt Chart generation

--- program30.c
+++ program30.c
@@ -12,3 +12,28 @@ int main() {
         printf("Enter Arrival Time and Burst Time for P%d: ", i + 1);
         scanf("%d%d", &at[i], &bt[i]);
     }
+ printf("\nGantt Chart:\n");
+
+    while (count < n) {
+        int min_bt = 9999, index = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (at[i] <= time && !completed[i] && bt[i] < min_bt) {
+                min_bt = bt[i];
+                index = i;
+            }
+        }
+
+        if (index == -1) {
+            time++;
+            continue;
+        }
+
+        printf("| P%d ", index + 1);
+        time += bt[index];
+        ct[index] = time;
+        tat[index] = ct[index] - at[index];
+        wt[index] = tat[index] - bt[index];
+        completed[index] = 1;
+        count++;
+    }
