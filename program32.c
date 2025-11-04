25d6a3b4294bb2f8168658edc24225985af9a85c
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-04 21:09:39 +0530

    Implemented parent process to write data to pipe

--- program32.c
+++ program32.c
@@ -13,3 +13,9 @@ int main() {
         read(pipefd[0], buffer, sizeof(buffer));
         printf("Child received: %s\n", buffer);
         close(pipefd[0]);
+} else {
+        close(pipefd[0]); 
+        char msg[] = "Hello from parent";
+        write(pipefd[1], msg, strlen(msg) + 1);
+        close(pipefd[1]);
+    }
